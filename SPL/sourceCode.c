f3329c393afd111dd2176c0c3d3b4fd76a2e6891
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2023-10-17 00:58:33 +0600

    vugichugi

--- SPL/sourceCode.c
+++ SPL/sourceCode.c
@@ -1,32 +1,33 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
 
 int main()
 {
     float x=11;
-    printf("%f",x);
+    printf("%f\n",x);
     float y=15;
     int z;
-    scanf("%d",&z);
+    //scanf("%d",&z);
 
-    float sum=x+y;
-
-    for(int i=1;i<=10;i++){
-        printf("%d\n",i);
+    int n=6;
+    //scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        //scanf("%d",&arr[i]);
     }
+
+    for(int i=1;i<=3;i++){
+        printf("%d ",i);
+    }
+    printf("\n");
     int j=0;
-    while(j<5){
-        printf("%d",j);
+    while(j<3){
+        printf("%d\n",j);
         j++;
     }
     if(x!=10){
-        printf("HelloWorld");
+        printf("HelloWorld\n");
     }
     else{
-        printf("TataWorld");
+        printf("TataWorld\n");
     }
-
-    return 0;
 }
